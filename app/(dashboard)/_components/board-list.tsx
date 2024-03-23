499e0b4fa61963ac6dcb6163a1fe7581ebd85eaa
Shubham Sharma <shubhamku044@gmail.com>
2024-03-23 16:31:23 +0530

    api added

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -1,4 +1,9 @@
+'use client';
 import { EmptyBoard, EmptyFavorite, EmptySearch } from '.';
+import { useQuery } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+import { BoardCard } from './board-card';
+import { NewBoardButton } from './new-board-button';
 
 interface IProps {
   orgId: string;
@@ -8,14 +13,54 @@ interface IProps {
   };
 }
 const BoardList = ({ orgId, query }: IProps) => {
-  const data = [];
+  const data = useQuery(api.boards.get, { orgId });
+
+  if (data === undefined)
+    return (
+      <div>
+        <h2 className="text-3xl">
+          {query.favorites ? 'Favorite boards' : 'Team boards'}
+        </h2>
+        <div className="mt-8 grid grid-cols-1 gap-5 pb-10 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+          <NewBoardButton disabled orgId={orgId} />
+          {new Array(6).fill(null).map((_, i) => (
+            <BoardCard.Skeleton key={i} />
+          ))}
+        </div>
+      </div>
+    );
+
   if (!data?.length && query.search) return <EmptySearch />;
 
   if (!data?.length && query.favorites) return <EmptyFavorite />;
 
   if (!data?.length) return <EmptyBoard />;
 
-  return <div>Board list compo {JSON.stringify(query)}</div>;
+  return (
+    <div>
+      <h2 className="text-3xl">
+        {query.favorites ? 'Favorite boards' : 'Team boards'}
+      </h2>
+      <div className="mt-8 grid grid-cols-1 gap-5 pb-10 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+        <NewBoardButton disabled={false} orgId={orgId} />
+        {data?.map((board) => {
+          return (
+            <BoardCard
+              key={board._id}
+              id={board._id}
+              title={board.title}
+              imageUrl={board.imageUrl}
+              authorName={board.authorName}
+              authorId={board.authorId}
+              orgId={board.orgId}
+              createdAt={board._creationTime}
+              isFavorite={false}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default BoardList;
--- app/(dashboard)/_components/empty-board.tsx
+++ app/(dashboard)/_components/empty-board.tsx
@@ -1,7 +1,29 @@
+'use client';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
+import { api } from '@/convex/_generated/api';
+import { useOrganization } from '@clerk/nextjs';
+import { useApiMutation } from '@/hooks/use-api-mutation';
+import { toast } from 'sonner';
 
 const EmptyBoard = () => {
+  const { organization } = useOrganization();
+  const { mutate, pending } = useApiMutation(api.board.create);
+
+  const onClick = () => {
+    if (!organization) return;
+    mutate({
+      orgId: organization.id,
+      title: 'New board',
+    })
+      .then((id) => {
+        toast.success('Board created successfully');
+      })
+      .catch(() => {
+        console.error('Failed to create board');
+        toast.error('Failed to create board');
+      });
+  };
   return (
     <div className="flex h-full flex-col items-center justify-center">
       <Image src="/note.svg" height={110} width={110} alt="Empty" />
@@ -10,7 +32,9 @@ const EmptyBoard = () => {
         Start by creating a board for your organization
       </p>
       <div className="mt-6">
-        <Button size="lg">Create board</Button>
+        <Button disabled={pending} onClick={onClick} size="lg">
+          Create board
+        </Button>
       </div>
     </div>
   );
